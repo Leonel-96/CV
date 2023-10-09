4f31fdda0e3de8c6ab0ff45139eaf476ec47e333
Leonel-96 <sidjouleonel@gmail.com>
2023-10-09 06:40:27 -0400

    v.1.1

--- js/script.js
+++ js/script.js
@@ -4,3 +4,22 @@ const navLinks = document.querySelector(".nav-links");
 menuHamburger.addEventListener('click',()=>{
     navLinks.classList.toggle('mobile-menu')
 });
+
+
+window.addEventListener('scroll',function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i = 0; i < reveals.length; i++){
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+} );
